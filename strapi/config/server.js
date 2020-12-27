600df211f467f6c275d926f9d79af053fb9e6e7f
Guilherme Araújo <guilherme.araujo@imd.ufrn.br>
2020-12-27 15:44:28 -0300

    Changing admin prefix

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -5,15 +5,6 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd07b79878484c62d0a9bcaf1da56dd1b'),
     },
-    path: '/workshop2020-admin',
-    build: {
-      backend: "https://bioinfo.imd.ufrn.br/workshop2020-api"
-    }
-  },
-  proxy: {
-    enabled: true,
-    host: "bioinfo.imd.ufrn.br/workshop2020-api",
-    port: 443,
-    ssl: true
+    url: 'workshop2020-admin'
   }
 });
